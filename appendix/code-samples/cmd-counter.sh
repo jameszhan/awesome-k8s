bf0ec704eb5479f9c7e16edf38401e8265d9a429
James Zhan <zhiqiangzhan@gmail.com>
2021-07-11 12:11:43 +0800

    Add Notes For shell脚本实战（第2版）

---
+++ appendix/code-samples/cmd-counter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IFS=":"
+count=0 ; nonex=0
+for directory in $PATH ;  do  
+    if [ -d "$directory" ] ; then    
+        for command in "$directory"/* ; do      
+            if [ -x "$command" ] ; then        
+                count="$(( $count + 1 ))"      
+            else        
+                nonex="$(( $nonex + 1 ))"      
+            fi    
+        done  
+    fi
+done
+
+echo "$count commands, and $nonex entries that weren't executable"
+exit 0
